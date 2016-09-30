368b7301f4b193c4b7d7df7eae8e5b23377fe2d5
rgravem <rgravem@yahoo.com>
2016-09-30 14:37:33 -0500

    finished

--- queries.js
+++ queries.js
@@ -9,6 +9,6 @@ db.bios.find({"awards.award":{$in:["Turing Award"]}}).pretty();
 // Find documents with IDs between 3 and 7.
 db.bios.find({_id:{$gt:2, $lt:8}}).pretty();
 // Find documents with awards that were awarded before the year 2000.
-db.bios.find({"awards.year":{$lt:2003}}).pretty();
+db.bios.find({"awards.year":{$lt:2003}})
 // Find documents with birth dates, but no death dates
 db.bios.find({$and:[{birth: {$exists: true}}, {death: {$exists: false}}]}).pretty();
